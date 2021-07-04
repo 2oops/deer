d88a9da56dcbb829680b991f22a515af1265b00f
2oops <yxlaimee@163.com>
2021-07-04 17:12:48 +0800

    feat: react-three/fiber

---
+++ src/threejs/SingleBox.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { OrbitControls, Stars } from '@react-three/drei';
+import { Physics, useBox, usePlane } from '@react-three/cannon'
+import { Canvas } from "@react-three/fiber";
+
+function Box() {
+  const [ref, api] = useBox(() => ({
+    mass: 1,
+    position: [0, 2, 0]
+  }))
+
+  return (
+    <mesh ref={ref} onClick={() => {
+      api.velocity.set(0, 2, 0)
+    }} >
+      <boxBufferGeometry attach="geometry" />
+      <meshLambertMaterial attach="material" color="hotpink" />
+    </mesh>
+  )
+}
+
+function Plane() {
+  const [ref] = usePlane(() => ({
+    rotation: [-Math.PI / 2, 0, 0]
+  }))
+
+  return (
+    <mesh ref={ref}>
+      <planeBufferGeometry attach="geometry" args={[100, 100]} />
+      <meshLambertMaterial attach="material" color="lightblue" />
+    </mesh>
+  )
+}
+
+function SingleBox() {
+  return (
+    <Canvas>
+      <OrbitControls />
+      <Stars />
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 15, 10]} angle={0.3} />
+      <Physics>
+        <Box />
+        <Plane />
+      </Physics>
+    </Canvas>
+  )
+}
+
+export default SingleBox
+
+
